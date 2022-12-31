4f62f2820d140ec87bdcf62a2f3c0bcd820e6eb5
Byte | Android Developer <117456618+3xByte@users.noreply.github.com>
2022-12-31 14:19:37 +0330

    Add files via upload

---
+++ contracts/NftErc1155.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+
+/**
+
+██████╗ ██╗  ██╗██████╗ ██╗   ██╗████████╗███████╗
+╚════██╗╚██╗██╔╝██╔══██╗╚██╗ ██╔╝╚══██╔══╝██╔════╝
+ █████╔╝ ╚███╔╝ ██████╔╝ ╚████╔╝    ██║   █████╗  
+ ╚═══██╗ ██╔██╗ ██╔══██╗  ╚██╔╝     ██║   ██╔══╝  
+██████╔╝██╔╝ ██╗██████╔╝   ██║      ██║   ███████╗
+╚═════╝ ╚═╝  ╚═╝╚═════╝    ╚═╝      ╚═╝   ╚══════╝
+                                                  
+
+Airdrop Marketing : https://github.com/3xByte/Airdrop-Marketing  
+
+*/
+
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/ERC1155.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+contract x3Byte is ERC1155, Ownable {
+    
+  string public name;
+  string public symbol;
+
+  mapping(uint => string) public tokenURI;
+
+  constructor() ERC1155("") {
+    name = "3xByte";
+    symbol = "3XB";
+  }
+
+  function mint(address _to, uint _id, uint _amount) external onlyOwner {
+    _mint(_to, _id, _amount, "");
+  }
+
+  function mintBatch(address _to, uint[] memory _ids, uint[] memory _amounts) external onlyOwner {
+    _mintBatch(_to, _ids, _amounts, "");
+  }
+
+  function burn(uint _id, uint _amount) external {
+    _burn(msg.sender, _id, _amount);
+  }
+
+  function burnBatch(uint[] memory _ids, uint[] memory _amounts) external {
+    _burnBatch(msg.sender, _ids, _amounts);
+  }
+
+  function burnForMint(address _from, uint[] memory _burnIds, uint[] memory _burnAmounts, uint[] memory _mintIds, uint[] memory _mintAmounts) external onlyOwner {
+    _burnBatch(_from, _burnIds, _burnAmounts);
+    _mintBatch(_from, _mintIds, _mintAmounts, "");
+  }
+
+  function setURI(uint _id, string memory _uri) external onlyOwner {
+    tokenURI[_id] = _uri;
+    emit URI(_uri, _id);
+  }
+
+  function uri(uint _id) public override view returns (string memory) {
+    return tokenURI[_id];
+  }
+
+}
+    
